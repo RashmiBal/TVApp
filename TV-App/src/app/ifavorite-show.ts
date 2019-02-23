36c623923aef720c95886a4bbd73300a690a1da7
OlgaHi <46238682+OlgaHi@users.noreply.github.com>
2019-02-23 15:44:31 -0800

    Interface file

---
+++ TV-App/src/app/ifavorite-show.ts
@@ -0,0 +1,12 @@
+
+export interface IFavoriteShow {
+    name: string
+    language: string
+    genres: [string]
+    schedule: [{
+        time:number,
+        days:string}]
+    image: string
+    description: string
+
+}
