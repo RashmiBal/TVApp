7404079cdd2c725ad7e2ecde8635517132897816
OlgaHi <46238682+OlgaHi@users.noreply.github.com>
2019-02-23 16:08:46 -0800

    All files

--- TV-App/src/app/ifavorite-show.ts
+++ TV-App/src/app/ifavorite-show.ts
@@ -2,10 +2,9 @@
 export interface IFavoriteShow {
     name: string
     language: string
-    genres: [string]
-    schedule: [{
-        time:number,
-        days:string}]
+    genres: string
+    schedule: string
+    days: string
     image: string
     description: string
 
