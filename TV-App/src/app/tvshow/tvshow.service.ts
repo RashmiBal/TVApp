2655864ca59cc16c25bb40c7cf2a36b6f1813db6
RashmiBal <rashmibaliga@outlook.com>
2019-02-23 22:13:07 -0800

    FourthModifications

--- TV-App/src/app/tvshow/tvshow.service.ts
+++ TV-App/src/app/tvshow/tvshow.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {environment} from './../../../src/environments/environment'
+
 
 interface ITvShowSearchData{
     score: number
@@ -21,4 +23,8 @@ interface ITvShowSearchData{
 export class TvshowService {
 
   constructor(private httpClient:HttpClient) { }
+
+  getTVShowsearchlist(userSearchText: string){
+    return this.httpClient.get<ITvShowSearchData>(`${environment.baseUrl}api.tvmaze.com/search/shows?q=${userSearchText}&appid=${environment.appId}`)
+  }
 }
