870ccbd9769db376f15adcd6df25dfdb78e2e8f3
Gmu <gmugmu@gmail.com>
2019-03-10 00:01:57 -0800

    Made all items returned display

--- TV-App/src/app/tvshow/tvshow.service.ts
+++ TV-App/src/app/tvshow/tvshow.service.ts
@@ -4,6 +4,7 @@ import {environment} from './../../../src/environments/environment'
 import { IFavoriteShow } from '../ifavorite-show';
 import {map} from 'rxjs/operators';
 
+/*
 interface ITvShowSearchData{
   0:{  
     show: {
@@ -22,8 +23,32 @@ interface ITvShowSearchData{
       }
     } 
   }
-}
+}*/
 
+interface ITvShowSearchData{
+  shows: Array<Show>;
+}
+export interface Show {
+  
+    score:number,
+    show: {
+      name: string
+      summary: string
+      runtime: number
+      officialSite: string
+      url: string
+      language: string
+      genres: [string]
+      image: {
+        medium: string}
+      schedule: {
+        time: string, 
+        days: [string]
+      }
+    }
+}
+ 
+  
 @Injectable({
   providedIn: 'root'
 })
@@ -32,13 +57,67 @@ export class TvshowService {
   constructor(private httpClient:HttpClient) { }
 
   getTVShowsearchlist(userSearchText: string | number){
+
+    let retreivedObservable  = this.httpClient.get<ITvShowSearchData>
+    (`${environment.baseUrl}api.tvmaze.com/search/shows?q=${userSearchText}&appId=${environment.appId}`);
+
+   //retreivedObservable.subscribe(x => console.log("retreived = "+x[0].show.name)); 
+//    retreivedObservable.pipe(map(x => console.log("retreived = "+x[0].show.name))); 
+//   return retreivedObservable.pipe(map(x => this.transformToIFavoriteShow(x))) 
+   return retreivedObservable.pipe(map(x => this.transformToIFavoriteShowsArray(x))) 
+
+/* ORIGINAL--
     return this.httpClient.get<ITvShowSearchData>
     (`${environment.baseUrl}api.tvmaze.com/search/shows?q=${userSearchText}&appId=${environment.appId}`).pipe(
       map(data => this.transformToIFavoriteShow(data))
     )
+    */
   }
 
-  private transformToIFavoriteShow(data: ITvShowSearchData): IFavoriteShow{
+  private transformToIFavoriteShowsArray(data: ITvShowSearchData): Array<IFavoriteShow>{
+
+    if (data[0] == undefined) return null;
+
+
+    console.log("retreived "+data[0].show.name)
+    
+    
+
+    var toRet: IFavoriteShow[]=[];
+    for (var i=0; (data[i] != undefined) && (data[i].show != undefined)&&((data[i].show != null));i++) {
+      console.log("retreived "+data[i].show.name)
+
+      if (data[i].show == undefined) continue;
+      if (data[i].show.image == null) continue;
+      if (data[i].show.image.medium == null) continue;
+
+      
+
+    toRet.push({
+      name: data[i].show.name,
+      summary: data[i].show.summary,
+      runtime: data[i].show.runtime,
+      officialSite: data[i].show.officialSite,
+      url: data[i].show.url,
+      language: data[i].show.language,
+      schedule: data[i].show.schedule.time,
+      days: data[i].show.schedule.days[i],
+      genres: data[i].show.genres[i],
+      image: data[i].show.image.medium
+    });
+  }
+
+
+
+    
+    return toRet;
+}
+
+
+private transformToIFavoriteShow(data: ITvShowSearchData): IFavoriteShow{
+
+    if (data[0] == undefined) return null;
+
     return {
       name: data[0].show.name,
       summary: data[0].show.summary,
